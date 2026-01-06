b7702d8dc712449951d5aef04bc8f63d29151e83
cmp-star <chandrashekharpujarm@gmail.com>
2026-01-06 19:07:30 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const questions = [
+  {
+    q: "Capital of India?",
+    options: ["Delhi", "Mumbai", "Chennai", "Kolkata"],
+    answer: 0
+  },
+  {
+    q: "2 + 3 = ?",
+    options: ["4", "5", "6", "7"],
+    answer: 1
+  }
+];
